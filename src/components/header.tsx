01658b0b86fb160a48c6ac166c7fcb1dc97ada4b
aida kashizadeh <aidakashizadeh@gmail.com>
2025-01-13 16:41:51 +0330

    header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,61 @@
-export default function Header(){
-    return(
-    <header className="bg-gray-200 flex h-96 justify-center items-center w-full text-sm md:text-base lg:text-lg xl:text-xl">
-      Header
-    </header>
-  )
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import Header1 from "../assets/images/header-min.png"; 
+import { Pagination } from 'swiper/modules';
+
+export default function Header() {
+  return (
+    <>
+      <header className="bg-gray-200 flex h-96 justify-center items-center w-full text-sm md:text-base lg:text-lg xl:text-xl">
+        <Swiper
+          spaceBetween={30}
+          pagination={{
+            dynamicBullets: true,
+            clickable: true, // Allow clicking on pagination bullets
+          }}
+          modules={[Pagination]}
+          className="w-full h-full"
+        >
+          <SwiperSlide>
+            <img
+              src={Header1.src}
+              alt="Header Image 1"
+              className="w-full h-full object-cover"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-blue-500">
+              Slide 2
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-green-500">
+              Slide 3
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-yellow-500">
+              Slide 4
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-red-500">
+              Slide 5
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-purple-500">
+              Slide 6
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full h-full flex justify-center items-center bg-pink-500">
+              Slide 7
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      </header>
+    </>
+  );
 }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,12 +3,32 @@ import React, { useContext } from 'react';
 import { SidebarContext } from '../contexts/sidebarContext';
 
 const Sidebar = () => {
-  const { isSidebarOpen } = useContext(SidebarContext);
+  const { isSidebarOpen,closeSidebar } = useContext(SidebarContext);
 
   return (
-    <aside className='z-10 absolute'>
-        {isSidebarOpen && <p className='bg-red-100 md:hidden'>Sidebar Content</p>}
-    </aside>
+    <div>
+  {/* Backdrop */}
+  {isSidebarOpen && (
+    <div
+      className="fixed inset-0 z-40 bg-black bg-opacity-50 transition-opacity duration-150 ease-in-out"
+      style={{
+        opacity: isSidebarOpen ? 100 : 0,
+      }}
+      onClick={closeSidebar}
+    />
+  )}
+
+  {/* Sidebar */}
+  <div
+    className="fixed inset-y-0 z-50 flex-shrink-0 w-64 mt-10 overflow-y-auto bg-white dark:bg-gray-800 lg:hidden transition-transform duration-150 ease-in-out"
+    style={{
+      transform: isSidebarOpen ? "translateX(0)" : "-translateX(20rem)",
+      opacity: isSidebarOpen ? 100 : 0,
+    }}
+  >
+    {/* content */}
+  </div>
+</div>
   );
 };
 
