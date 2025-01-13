71e2cbd62a64b96cbfb89f447ab553136129f925
aida kashizadeh <aidakashizadeh@gmail.com>
2025-01-13 18:22:14 +0330

    menu

---
+++ src/components/desktopMenu.tsx
@@ -0,0 +1,14 @@
+
+export default function DesktopMenu(){
+  return(
+    <>
+    <ul>
+        <li><a>Home</a></li>
+        <br/>
+        <li><a>dropDown</a></li>
+        <br/>
+        <li><a>about us</a></li>
+    </ul>
+    </>
+  )
+}
